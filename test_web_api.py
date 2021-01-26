d48e579ed237a85198d254efa78c6d343e9e0969
Aruelius <25380989+Aruelius@users.noreply.github.com>
2021-01-27 00:36:35 +0800

    first commit

---
+++ test_web_api.py
@@ -0,0 +1,29 @@
+import os
+
+import tensorflow as tf
+from flask import Flask, jsonify, request
+from PIL import Image
+
+from yolo import YOLO
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+app = Flask(__name__)
+yolox = YOLO()
+
+
+@app.route("/test", methods=["POST"])
+def test():
+    jpgfile = request.files.get("file")
+    if not jpgfile:
+        return jsonify(code=403, msg="empty file")
+    img = Image.open(jpgfile)
+    result = yolox.detect_image(img, distance=True)
+    return jsonify(code=0, data=result, msg="success")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
